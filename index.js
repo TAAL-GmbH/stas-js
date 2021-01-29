0fb54112bed595272c14ae51d8f952678237c5d6
Simon Ordish <simon.ordish@masagi.co.uk>
2021-01-29 16:10:01 +0000

    Added merge functionality

--- index.js
+++ index.js
@@ -8,5 +8,6 @@ stas.transfer = require('./lib/transfer')
 stas.split = require('./lib/split')
 stas.redeemSplit = require('./lib/redeemSplit')
 stas.redeem = require('./lib/redeem')
+stas.merge = require('./lib/merge')
 
 stas.utils = require('./lib/utils')
---
+++ lifecycleV2.test.js
@@ -0,0 +1,173 @@
+const bsv = require('bsv')
+
+const {
+  contract,
+  issue,
+  transfer,
+  split,
+  merge,
+  redeem
+} = require('./index')
+
+const {
+  getTransaction,
+  getFundsFromFaucet,
+  broadcast
+} = require('./index').utils
+
+;(async () => {
+  const issuerPrivateKey = bsv.PrivateKey()
+  const alicePrivateKey = bsv.PrivateKey()
+  const bobPrivateKey = bsv.PrivateKey()
+
+  const utxos = await getFundsFromFaucet(issuerPrivateKey.toAddress('testnet').toString())
+
+  const publicKeyHash = bsv.crypto.Hash.sha256ripemd160(issuerPrivateKey.publicKey.toBuffer()).toString('hex')
+
+  const schema = {
+    schemaId: 'Schema STAS Coupon',
+    tokenName: 'TAALT',
+    tokenId: publicKeyHash,
+    tokenDescription: 'Example token on private Taalnet',
+    issuerName: 'Taal Technologies SEZC',
+    issuerCountry: 'CYM',
+    issuerLegalForm: 'Limited Liability Public Company',
+    issuerEmail: 'info@taal.com',
+    issuerWebsite: 'https://taal.com',
+    terms: '© 2020 TAAL TECHNOLOGIES SEZC\nALL RIGHTS RESERVED. ANY USE OF THIS SOFTWARE IS SUBJECT TO TERMS AND CONDITIONS OF LICENSE. USE OF THIS SOFTWARE WITHOUT LICENSE CONSTITUTES INFRINGEMENT OF INTELLECTUAL PROPERTY. FOR LICENSE DETAILS OF THE SOFTWARE, PLEASE REFER TO: www.taal.com/stas-token-license-agreement',
+    governingLaw: 'Cayman Islands Law',
+    icon: 'https://www.taal.com/wp-content/themes/taal_v2/img/favicon/favicon-96x96.png',
+    tickerSymbol: 'TAALT'
+  }
+
+  const contractHex = contract(
+    issuerPrivateKey,
+    utxos,
+    schema,
+    10000
+  )
+  const contractTxid = await broadcast(contractHex)
+  console.log(`Contract TX:     ${contractTxid}`)
+  const contractTx = await getTransaction(contractTxid)
+
+  const issueHex = issue(
+    issuerPrivateKey,
+    {
+      txid: contractTxid,
+      vout: 0,
+      scriptPubKey: contractTx.vout[0].scriptPubKey.hex,
+      amount: contractTx.vout[0].value
+    },
+    [{
+      txid: contractTxid,
+      vout: 1,
+      scriptPubKey: contractTx.vout[1].scriptPubKey.hex,
+      amount: contractTx.vout[1].value
+    }],
+    2 // STAS version
+  )
+  const issueTxid = await broadcast(issueHex)
+  console.log(`Issue TX:        ${issueTxid}`)
+  const issueTx = await getTransaction(issueTxid)
+
+  const transferHex = transfer(
+    issuerPrivateKey,
+    issuerPrivateKey.publicKey,
+    {
+      txid: issueTxid,
+      vout: 0,
+      scriptPubKey: issueTx.vout[0].scriptPubKey.hex,
+      amount: issueTx.vout[0].value
+    },
+    alicePrivateKey.publicKey,
+    [{
+      txid: issueTxid,
+      vout: 1,
+      scriptPubKey: issueTx.vout[1].scriptPubKey.hex,
+      amount: issueTx.vout[1].value
+    }],
+    issuerPrivateKey
+  )
+  const transferTxid = await broadcast(transferHex)
+  console.log(`Transfer TX:     ${transferTxid}`)
+  const transferTx = await getTransaction(transferTxid)
+
+  // Split tokens into 2 - both payable to Bob...
+  const bobAmount1 = transferTx.vout[0].value / 2
+  const bobAmount2 = transferTx.vout[0].value - bobAmount1
+
+  const splitHex = split(
+    alicePrivateKey,
+    issuerPrivateKey.publicKey,
+    {
+      txid: transferTxid,
+      vout: 0,
+      scriptPubKey: transferTx.vout[0].scriptPubKey.hex,
+      amount: transferTx.vout[0].value
+    },
+    bobPrivateKey.publicKey,
+    bobAmount1,
+    bobPrivateKey.publicKey,
+    bobAmount2,
+    [{
+      txid: transferTxid,
+      vout: 1,
+      scriptPubKey: transferTx.vout[1].scriptPubKey.hex,
+      amount: transferTx.vout[1].value
+    }],
+    issuerPrivateKey
+  )
+  const splitTxid = await broadcast(splitHex)
+  console.log(`Split TX:        ${splitTxid}`)
+  const splitTx = await getTransaction(splitTxid)
+
+  // Now let's merge the last split back together
+  const splitTxObj = new bsv.Transaction(splitHex)
+
+  const mergeHex = merge(
+    bobPrivateKey,
+    issuerPrivateKey.publicKey,
+    [{
+      tx: splitTxObj,
+      vout: 0
+    },
+    {
+      tx: splitTxObj,
+      vout: 1
+    }],
+    alicePrivateKey.publicKey,
+    {
+      txid: splitTxid,
+      vout: 2,
+      scriptPubKey: splitTx.vout[2].scriptPubKey.hex,
+      amount: splitTx.vout[2].value
+    },
+    issuerPrivateKey
+  )
+
+  const mergeTxid = await broadcast(mergeHex)
+  console.log(`Merge TX:        ${mergeTxid}`)
+  const mergeTx = await getTransaction(mergeTxid)
+
+  // Alice wants to redeem the money from bob...
+  const redeemHex = redeem(
+    alicePrivateKey,
+    issuerPrivateKey.publicKey,
+    {
+      txid: mergeTxid,
+      vout: 0,
+      scriptPubKey: mergeTx.vout[0].scriptPubKey.hex,
+      amount: mergeTx.vout[0].value
+    },
+    [{
+      txid: mergeTxid,
+      vout: 1,
+      scriptPubKey: mergeTx.vout[1].scriptPubKey.hex,
+      amount: mergeTx.vout[1].value
+    }],
+    issuerPrivateKey
+  )
+  const redeemTxid = await broadcast(redeemHex)
+  console.log(`Redeem TX:       ${redeemTxid}`)
+  // const redeem1Tx = await getTransaction(redeem1Txid)
+})()
