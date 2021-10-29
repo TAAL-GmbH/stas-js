80f84864bff0b60e4166b9f7004a009874ed591d
Leonard Collins <leonard@taal.com>
2021-10-29 23:27:50 +0100

    DEVP-1194 added tests

---
+++ test/datasize_tests.js
@@ -0,0 +1,320 @@
+const expect = require('chai').expect
+const utils = require('./utils/test_utils')
+const bsv = require('bsv')
+require('dotenv').config()
+
+const {
+  contract,
+  issue
+} = require('./../index')
+
+const {
+  getTransaction,
+  getFundsFromFaucet,
+  broadcast,
+
+} = require('./../index').utils
+
+it('Data Size - < 128 Bytes', async function () {
+    const issuerPrivateKey = bsv.PrivateKey()
+    const fundingPrivateKey = bsv.PrivateKey()
+  
+    const alicePrivateKey = bsv.PrivateKey()
+    const aliceAddr = alicePrivateKey.toAddress(process.env.NETWORK).toString()
+  
+    const bobPrivateKey = bsv.PrivateKey()
+    const bobAddr = bobPrivateKey.toAddress(process.env.NETWORK).toString()
+  
+    const contractUtxos = await getFundsFromFaucet(issuerPrivateKey.toAddress(process.env.NETWORK).toString())
+    const fundingUtxos = await getFundsFromFaucet(fundingPrivateKey.toAddress(process.env.NETWORK).toString())
+  
+    const publicKeyHash = bsv.crypto.Hash.sha256ripemd160(issuerPrivateKey.publicKey.toBuffer()).toString('hex')
+    const supply = 10000
+    const symbol = 'TAALT'
+    const schema = utils.schema(publicKeyHash, symbol, supply)
+
+    console.log('Symbol  ' + symbol)
+
+    const contractHex = contract(
+      issuerPrivateKey,
+      contractUtxos,
+      fundingUtxos,
+      fundingPrivateKey,
+      schema,
+      supply
+    )
+    const contractTxid = await broadcast(contractHex)
+    console.log(`Contract TX:     ${contractTxid}`)
+    const contractTx = await getTransaction(contractTxid)
+  
+  let data = 'It was the best of times, it was the worst of times, it was the age of wisdom. It was the best of times, it was the'
+  console.log("Data Size " + byteCount(data))
+    const issueInfo = [
+      {
+        addr: aliceAddr,
+        satoshis: 10000,
+        data: data
+      }
+    ]
+    const issueHex = issue(
+      issuerPrivateKey,
+      issueInfo,
+      utils.getUtxo(contractTxid, contractTx, 0),
+      utils.getUtxo(contractTxid, contractTx, 1),
+      fundingPrivateKey,
+      true,
+      symbol,
+      2
+    )
+    const issueTxid = await broadcast(issueHex)
+    const tokenId = await utils.getToken(issueTxid)
+    console.log(`issueTxid:        ${issueTxid}`)
+    console.log(`Token ID:        ${tokenId}`)
+    const response = await utils.getTokenResponse(tokenId) // token issuance fails intermittingly
+    expect(response.symbol).to.equal(symbol)
+  })
+
+
+it('Data Size > 128 Bytes', async function () {
+  const issuerPrivateKey = bsv.PrivateKey()
+  const fundingPrivateKey = bsv.PrivateKey()
+
+  const alicePrivateKey = bsv.PrivateKey()
+  const aliceAddr = alicePrivateKey.toAddress(process.env.NETWORK).toString()
+
+  const bobPrivateKey = bsv.PrivateKey()
+  const bobAddr = bobPrivateKey.toAddress(process.env.NETWORK).toString()
+
+  const contractUtxos = await getFundsFromFaucet(issuerPrivateKey.toAddress(process.env.NETWORK).toString())
+  const fundingUtxos = await getFundsFromFaucet(fundingPrivateKey.toAddress(process.env.NETWORK).toString())
+
+  const publicKeyHash = bsv.crypto.Hash.sha256ripemd160(issuerPrivateKey.publicKey.toBuffer()).toString('hex')
+  const supply = 10000
+  const symbol = 'TAALT'
+  const schema = utils.schema(publicKeyHash, symbol, supply)
+  
+
+  console.log('Symbol  ' + symbol)
+
+  // change goes back to the fundingPrivateKey
+  const contractHex = contract(
+    issuerPrivateKey,
+    contractUtxos,
+    fundingUtxos,
+    fundingPrivateKey,
+    schema,
+    supply
+  )
+  const contractTxid = await broadcast(contractHex)
+  console.log(`Contract TX:     ${contractTxid}`)
+  const contractTx = await getTransaction(contractTxid)
+
+let data = 'It was the best of times, it was the worst of times, it was the age of wisdom. It was the best of times, it was the worst of'
+console.log("Data Size " + byteCount(data))
+  const issueInfo = [
+    {
+      addr: aliceAddr,
+      satoshis: 10000,
+      data: data
+    }
+  ]
+  const issueHex = issue(
+    issuerPrivateKey,
+    issueInfo,
+    utils.getUtxo(contractTxid, contractTx, 0),
+    utils.getUtxo(contractTxid, contractTx, 1),
+    fundingPrivateKey,
+    true,
+    symbol,
+    2
+  )
+  const issueTxid = await broadcast(issueHex)
+  const tokenId = await utils.getToken(issueTxid)
+  console.log(`issueTxid:        ${issueTxid}`)
+  console.log(`Token ID:        ${tokenId}`)
+  const response = await utils.getTokenResponse(tokenId) // token issuance fails intermittingly
+  expect(response.symbol).to.equal(symbol)
+})
+
+
+it('Data Size > 32768 Bytes', async function () {
+    const issuerPrivateKey = bsv.PrivateKey()
+    const fundingPrivateKey = bsv.PrivateKey()
+  
+    const alicePrivateKey = bsv.PrivateKey()
+    const aliceAddr = alicePrivateKey.toAddress(process.env.NETWORK).toString()
+
+    const contractUtxos = await getFundsFromFaucet(issuerPrivateKey.toAddress(process.env.NETWORK).toString())
+    const fundingUtxos = await getFundsFromFaucet(fundingPrivateKey.toAddress(process.env.NETWORK).toString())
+  
+    const publicKeyHash = bsv.crypto.Hash.sha256ripemd160(issuerPrivateKey.publicKey.toBuffer()).toString('hex')
+    const supply = 10000
+    const symbol = 'TAALT'
+    const schema = utils.schema(publicKeyHash, symbol, supply)
+  
+    console.log('Symbol  ' + symbol)
+  
+    // change goes back to the fundingPrivateKey
+    const contractHex = contract(
+      issuerPrivateKey,
+      contractUtxos,
+      fundingUtxos,
+      fundingPrivateKey,
+      schema,
+      supply
+    )
+    const contractTxid = await broadcast(contractHex)
+    console.log(`Contract TX:     ${contractTxid}`)
+    const contractTx = await getTransaction(contractTxid)
+  
+    const issueInfo = [
+      {
+        addr: aliceAddr,
+        satoshis: 10000,
+        data:  addData(33)
+      }
+    ]
+    const issueHex = issue(
+      issuerPrivateKey,
+      issueInfo,
+      utils.getUtxo(contractTxid, contractTx, 0),
+      utils.getUtxo(contractTxid, contractTx, 1),
+      fundingPrivateKey,
+      true,
+      symbol,
+      2
+    )
+    const issueTxid = await broadcast(issueHex)
+    const tokenId = await utils.getToken(issueTxid)
+    console.log(`issueTxid:        ${issueTxid}`)
+    console.log(`Token ID:        ${tokenId}`)
+    const response = await utils.getTokenResponse(tokenId) // token issuance fails intermittingly
+    expect(response.symbol).to.equal(symbol)
+  })
+  
+  it('Data Size < 32768 Bytes', async function () {
+    const issuerPrivateKey = bsv.PrivateKey()
+    const fundingPrivateKey = bsv.PrivateKey()
+  
+    const alicePrivateKey = bsv.PrivateKey()
+    const aliceAddr = alicePrivateKey.toAddress(process.env.NETWORK).toString()
+
+    const contractUtxos = await getFundsFromFaucet(issuerPrivateKey.toAddress(process.env.NETWORK).toString())
+    const fundingUtxos = await getFundsFromFaucet(fundingPrivateKey.toAddress(process.env.NETWORK).toString())
+  
+    const publicKeyHash = bsv.crypto.Hash.sha256ripemd160(issuerPrivateKey.publicKey.toBuffer()).toString('hex')
+    const supply = 10000
+    const symbol = 'TAALT'
+    const schema = utils.schema(publicKeyHash, symbol, supply)
+  
+    console.log('Symbol  ' + symbol)
+  
+    // change goes back to the fundingPrivateKey
+    const contractHex = contract(
+      issuerPrivateKey,
+      contractUtxos,
+      fundingUtxos,
+      fundingPrivateKey,
+      schema,
+      supply
+    )
+    const contractTxid = await broadcast(contractHex)
+    console.log(`Contract TX:     ${contractTxid}`)
+    const contractTx = await getTransaction(contractTxid)
+  
+    const issueInfo = [
+      {
+        addr: aliceAddr,
+        satoshis: 10000,
+        data: addData(32)
+      }
+    ]
+    const issueHex = issue(
+      issuerPrivateKey,
+      issueInfo,
+      utils.getUtxo(contractTxid, contractTx, 0),
+      utils.getUtxo(contractTxid, contractTx, 1),
+      fundingPrivateKey,
+      true,
+      symbol,
+      2
+    )
+    const issueTxid = await broadcast(issueHex)
+    const tokenId = await utils.getToken(issueTxid)
+    console.log(`issueTxid:        ${issueTxid}`)
+    console.log(`Token ID:        ${tokenId}`)
+    const response = await utils.getTokenResponse(tokenId) // token issuance fails intermittingly
+    expect(response.symbol).to.equal(symbol)
+  })
+
+
+  it('Data Size Large', async function () {
+    const issuerPrivateKey = bsv.PrivateKey()
+    const fundingPrivateKey = bsv.PrivateKey()
+  
+    const alicePrivateKey = bsv.PrivateKey()
+    const aliceAddr = alicePrivateKey.toAddress(process.env.NETWORK).toString()
+
+    const contractUtxos = await getFundsFromFaucet(issuerPrivateKey.toAddress(process.env.NETWORK).toString())
+    const fundingUtxos = await getFundsFromFaucet(fundingPrivateKey.toAddress(process.env.NETWORK).toString())
+  
+    const publicKeyHash = bsv.crypto.Hash.sha256ripemd160(issuerPrivateKey.publicKey.toBuffer()).toString('hex')
+    const supply = 10000
+    const symbol = 'TAALT'
+    const schema = utils.schema(publicKeyHash, symbol, supply)
+  
+    console.log('Symbol  ' + symbol)
+  
+    // change goes back to the fundingPrivateKey
+    const contractHex = contract(
+      issuerPrivateKey,
+      contractUtxos,
+      fundingUtxos,
+      fundingPrivateKey,
+      schema,
+      supply
+    )
+    const contractTxid = await broadcast(contractHex)
+    console.log(`Contract TX:     ${contractTxid}`)
+    const contractTx = await getTransaction(contractTxid)
+  
+    const issueInfo = [
+      {
+        addr: aliceAddr,
+        satoshis: 10000,
+        data: addData(1000)
+      }
+    ]
+    const issueHex = issue(
+      issuerPrivateKey,
+      issueInfo,
+      utils.getUtxo(contractTxid, contractTx, 0),
+      utils.getUtxo(contractTxid, contractTx, 1),
+      fundingPrivateKey,
+      true,
+      symbol,
+      2
+    )
+    const issueTxid = await broadcast(issueHex)
+    const tokenId = await utils.getToken(issueTxid)
+    console.log(`issueTxid:        ${issueTxid}`)
+    console.log(`Token ID:        ${tokenId}`)
+    const response = await utils.getTokenResponse(tokenId)
+    expect(response.symbol).to.equal(symbol)
+  })
+
+
+function addData(sizeInKB){
+
+    let data
+    for(let i = 0; i < sizeInKB; i++){
+    
+        data += 'It was the best of times, it was the worst of times, it was the age of wisdom. It was the best of times, it was the worst of, It was the best of times, it was the worst of times, it was the age of wisdom. It was the best of times, it was the worst of, It was the best of times, it was the worst of times, it was the age of wisdom. It was the best of times, it was the worst of, It was the best of times, it was the worst of times, it was the age of wisdom. It was the best of times, it was the worst of, It was the best of times, it was the worst of times, it was the age of wisdom. It was the best of times, it was the worst of, It was the best of times, it was the worst of times, it was the age of wisdom. It was the best of times, it was the worst of, It was the best of times, it was the worst of times, it was the age of wisdom. It was the best of times, it was the worst of, It was the best of times, it was the worst of times, it was the age of wisdom. It was the best of times, it was the and'
+    }
+    return data
+}
+
+  
+function byteCount(s) {
+  return encodeURI(s).split(/%..|./).length - 1;
+}
