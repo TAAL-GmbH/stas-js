e047d68a1cb669d4f5acd3b4d69126ffd013f005
Leonard Collins <leonard@taal.com>
2021-11-20 05:07:30 +0000

    DEVP-1194 updated tests

--- test/mainnet_tests/mainnet_lifecyle1_test.js
+++ test/mainnet_tests/mainnet_lifecyle1_test.js
@@ -22,11 +22,11 @@ it('Mainnet LifeCycle Test 1', async function () {
 
 
   // per-run modifiable values
-  const inputUtxoid = '327afa6665a2c314508e5b9a86efa873b019bc87607c89bbf8e216aa74dd1da2' // the input utxo
-  const inputUtxoIdVoutIndex = 14
-  const inputUtxoidFee = '327afa6665a2c314508e5b9a86efa873b019bc87607c89bbf8e216aa74dd1da2' // the fee utxo
-  const inputUtxoIdFeeVoutIndex = 15
-  const symbol = 'piero-23' // Use a unique symbol every test run to ensure that token balances can be checked correctly
+  const inputUtxoid = '' // the input utxo
+  const inputUtxoIdVoutIndex = 2
+  const inputUtxoidFee = '' // the fee utxo
+  const inputUtxoIdFeeVoutIndex = 23
+  const symbol = 'test-' + randomSymbol(10) // Use a unique symbol every test run to ensure that token balances can be checked correctly
 
   console.log('token symbol:', symbol)
 
@@ -34,7 +34,7 @@ it('Mainnet LifeCycle Test 1', async function () {
   const bobsInitialSathoshis = 6000
   const emmasInitialSatoshis = supply - bobsInitialSathoshis
 
-  const wait = 1000 // set wait before token balance check
+  const wait = 1000 // set wait before token balance check in case of delay
 
   const issuerWif = process.env.ALICEWIF // the issuer of the contract and pays fees
   const bobWif = process.env.BOBWIF
@@ -50,47 +50,11 @@ it('Mainnet LifeCycle Test 1', async function () {
   console.log('Emma Address ' + emmaAddr)
 
   const inputUtxo = await utils.getTransactionMainNet(inputUtxoid)
-  console.log(inputUtxo)
   const inputUtxoFee = await utils.getTransactionMainNet(inputUtxoidFee)
 
   const publicKeyHash = bsv.crypto.Hash.sha256ripemd160(issuerPrivateKey.publicKey.toBuffer()).toString('hex')
+  const schema = utils.schema(publicKeyHash, symbol, supply)
 
-  const schema = {
-    name: 'Taal Token',
-    tokenId: `${publicKeyHash}`,
-    protocolId: 'To be decided',
-    symbol: symbol,
-    description: 'Example token on private Taalnet',
-    image: 'https://www.taal.com/wp-content/themes/taal_v2/img/favicon/favicon-96x96.png',
-    totalSupply: supply,
-    decimals: 0,
-    satsPerToken: 1,
-    properties: {
-      legal: {
-        terms: '© 2020 TAAL TECHNOLOGIES SEZC\nALL RIGHTS RESERVED. ANY USE OF THIS SOFTWARE IS SUBJECT TO TERMS AND CONDITIONS OF LICENSE. USE OF THIS SOFTWARE WITHOUT LICENSE CONSTITUTES INFRINGEMENT OF INTELLECTUAL PROPERTY. FOR LICENSE DETAILS OF THE SOFTWARE, PLEASE REFER TO: www.taal.com/stas-token-license-agreement',
-        licenceId: '1234'
-      },
-      issuer: {
-        organisation: 'Taal Technologies SEZC',
-        legalForm: 'Limited Liability Public Company',
-        governingLaw: 'CA',
-        mailingAddress: '1 Volcano Stret, Canada',
-        issuerCountry: 'CYM',
-        jurisdiction: '',
-        email: 'info@taal.com'
-      },
-      meta: {
-        schemaId: 'token1',
-        website: 'https://taal.com',
-        legal: {
-          terms: 'blah blah'
-        },
-        media: {
-          type: 'mp4'
-        }
-      }
-    }
-  }
   const contractHex = contract(
     issuerPrivateKey,
     [{
@@ -164,7 +128,7 @@ it('Mainnet LifeCycle Test 1', async function () {
   console.log(`Token ID:        ${tokenId}`)
   const response = await utils.getTokenResponseMainNet(tokenId, symbol)
   expect(response.symbol).to.equal(symbol)
-
+  console.log("token issued")
   // eslint-disable-next-line promise/param-names
   await new Promise(r => setTimeout(r, wait))
 
@@ -247,10 +211,10 @@ it('Mainnet LifeCycle Test 1', async function () {
       tx: splitTxObj,
       vout: 0
     },
-      {
-        tx: splitTxObj,
-        vout: 1
-      }],
+    {
+      tx: splitTxObj,
+      vout: 1
+    }],
     emmaAddr,
     {
       txid: splitTxid,
@@ -318,13 +282,13 @@ it('Mainnet LifeCycle Test 1', async function () {
       vout: 0,
       amount: splitTx2.vout[0].value
     },
-      {
-        tx: splitTxObj2,
-        scriptPubKey: splitTx2.vout[1].scriptPubKey.hex,
-        vout: 1,
-        amount: splitTx2.vout[1].value
+    {
+      tx: splitTxObj2,
+      scriptPubKey: splitTx2.vout[1].scriptPubKey.hex,
+      vout: 1,
+      amount: splitTx2.vout[1].value
 
-      }],
+    }],
     bobAddr,
     aliceAmountSatoshis,
     emmaAddr,
@@ -370,5 +334,15 @@ it('Mainnet LifeCycle Test 1', async function () {
 
 })
 
+function randomSymbol(length) {
+  var result = '';
+  var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() *
+      charactersLength));
+  }
+  return result;
+}
 
 
