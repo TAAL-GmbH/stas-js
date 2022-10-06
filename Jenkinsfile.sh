d0a657f19ab4607e8e4d35075048bd2f8a425252
Leonard Collins <leonard@taal.com>
2022-10-06 09:57:30 +0100

    BPAAS-157 added jenkinsfile

---
+++ Jenkinsfile.sh
@@ -0,0 +1,32 @@
+Jenkinsfile (Declarative Pipeline)
+pipeline {
+    agent any
+
+    stages {
+
+        stage('Clone Repo') {
+            steps {
+                echo 'Cloning Repo'
+                sh 'git clone git@github.com:TAAL-GmbH/stas-js.git'
+            }
+        }
+        stage('Install Dependencies') {
+            steps {
+                echo 'Installing Dependencies..'
+                sh 'npm install'
+            }
+        }
+        stage('Unit Tests') {
+            steps {
+                echo 'Unit Test'
+                sh 'API_USERNAME=taal_private API_PASSWORD=dotheT@@l007 npm run test contractUnit'
+            }
+        }
+          stage('Functional Tests') {
+            steps {
+                echo 'Functional Tests'
+                sh 'API_USERNAME=taal_private API_PASSWORD=dotheT@@l007 npm run test'
+            }
+        }
+    }
+}
