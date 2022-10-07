cc6bb76bf0ad42b4ef690b65d7ca8ef28c76c5cb
Leonard Collins <leonard@taal.com>
2022-10-07 05:51:30 +0100

    BPAAS-551 added split unit tests

--- test/function_tests/split_tests2.js
+++ test/function_tests/split_tests2.js
@@ -1,267 +1,267 @@
-const expect = require('chai').expect
-const utils = require('../utils/test_utils')
-const bsv = require('bsv')
-require('dotenv').config()
+// const expect = require('chai').expect
+// const utils = require('../utils/test_utils')
+// const bsv = require('bsv')
+// require('dotenv').config()
 
-const {
-  contract,
-  issue,
-  split,
-  splitWithCallback
-} = require('../../index')
+// const {
+//   contract,
+//   issue,
+//   split,
+//   splitWithCallback
+// } = require('../../index')
 
-const {
-  bitcoinToSatoshis,
-  getTransaction,
-  getFundsFromFaucet,
-  broadcast
-} = require('../../index').utils
+// const {
+//   bitcoinToSatoshis,
+//   getTransaction,
+//   getFundsFromFaucet,
+//   broadcast
+// } = require('../../index').utils
 
-const { sighash } = require('../../lib/stas')
+// const { sighash } = require('../../lib/stas')
 
-let issuerPrivateKey
-let fundingPrivateKey
-let contractUtxos
-let fundingUtxos
-let publicKeyHash
-let bobPrivateKey
-let alicePrivateKey
-let bobAddr
-let aliceAddr
-let issueTxid
-let issueTx
+// let issuerPrivateKey
+// let fundingPrivateKey
+// let contractUtxos
+// let fundingUtxos
+// let publicKeyHash
+// let bobPrivateKey
+// let alicePrivateKey
+// let bobAddr
+// let aliceAddr
+// let issueTxid
+// let issueTx
 
-beforeAll(async () => {
-  await setup() // contract and issue
-})
+// beforeAll(async () => {
+//   await setup() // contract and issue
+// })
 
-it('Split - Splitting Into Too Many Tokens Throws Error', async () => {
-  const bobAmount = issueTx.vout[0].value / 5
-  const splitDestinations = []
-  splitDestinations[0] = { address: bobAddr, satoshis: bitcoinToSatoshis(bobAmount) }
-  splitDestinations[1] = { address: bobAddr, satoshis: bitcoinToSatoshis(bobAmount) }
-  splitDestinations[2] = { address: bobAddr, satoshis: bitcoinToSatoshis(bobAmount) }
-  splitDestinations[3] = { address: bobAddr, satoshis: bitcoinToSatoshis(bobAmount) }
-  splitDestinations[4] = { address: bobAddr, satoshis: bitcoinToSatoshis(bobAmount) }
-  try {
-    await split(
-      alicePrivateKey,
-      utils.getUtxo(issueTxid, issueTx, 0),
-      splitDestinations,
-      utils.getUtxo(issueTxid, issueTx, 2),
-      fundingPrivateKey
-    )
-    expect(false).toBeTruthy()
-    return
-  } catch (e) {
-    expect(e).to.be.instanceOf(Error)
-    expect(e.message).to.eql('Must have less than 5 segments')
-  }
-})
+// it('Split - Splitting Into Too Many Tokens Throws Error', async () => {
+//   const bobAmount = issueTx.vout[0].value / 5
+//   const splitDestinations = []
+//   splitDestinations[0] = { address: bobAddr, satoshis: bitcoinToSatoshis(bobAmount) }
+//   splitDestinations[1] = { address: bobAddr, satoshis: bitcoinToSatoshis(bobAmount) }
+//   splitDestinations[2] = { address: bobAddr, satoshis: bitcoinToSatoshis(bobAmount) }
+//   splitDestinations[3] = { address: bobAddr, satoshis: bitcoinToSatoshis(bobAmount) }
+//   splitDestinations[4] = { address: bobAddr, satoshis: bitcoinToSatoshis(bobAmount) }
+//   try {
+//     await split(
+//       alicePrivateKey,
+//       utils.getUtxo(issueTxid, issueTx, 0),
+//       splitDestinations,
+//       utils.getUtxo(issueTxid, issueTx, 2),
+//       fundingPrivateKey
+//     )
+//     expect(false).toBeTruthy()
+//     return
+//   } catch (e) {
+//     expect(e).to.be.instanceOf(Error)
+//     expect(e.message).to.eql('Must have less than 5 segments')
+//   }
+// })
 
-it('Split - Empty Array Split Throws Error', async () => {
-  const splitDestinations = []
-  try {
-    await split(
-      alicePrivateKey,
-      utils.getUtxo(issueTxid, issueTx, 0),
-      splitDestinations,
-      utils.getUtxo(issueTxid, issueTx, 2),
-      fundingPrivateKey
-    )
-    expect(false).toBeTruthy()
-    return
-  } catch (e) {
-    expect(e).to.be.instanceOf(Error)
-    expect(e.message).to.eql('split destinations array is null or empty')
-  }
-})
+// it('Split - Empty Array Split Throws Error', async () => {
+//   const splitDestinations = []
+//   try {
+//     await split(
+//       alicePrivateKey,
+//       utils.getUtxo(issueTxid, issueTx, 0),
+//       splitDestinations,
+//       utils.getUtxo(issueTxid, issueTx, 2),
+//       fundingPrivateKey
+//     )
+//     expect(false).toBeTruthy()
+//     return
+//   } catch (e) {
+//     expect(e).to.be.instanceOf(Error)
+//     expect(e.message).to.eql('split destinations array is null or empty')
+//   }
+// })
 
-it('Split - Add Zero Sats to Split Throws Error', async () => {
-  const splitDestinations = []
-  splitDestinations[0] = { address: bobAddr, satoshis: 0 }
-  splitDestinations[1] = { address: bobAddr, satoshis: 0 }
+// it('Split - Add Zero Sats to Split Throws Error', async () => {
+//   const splitDestinations = []
+//   splitDestinations[0] = { address: bobAddr, satoshis: 0 }
+//   splitDestinations[1] = { address: bobAddr, satoshis: 0 }
 
-  try {
-    await split(
-      alicePrivateKey,
-      utils.getUtxo(issueTxid, issueTx, 0),
-      splitDestinations,
-      utils.getUtxo(issueTxid, issueTx, 2),
-      fundingPrivateKey
-    )
-    expect(false).toBeTruthy()
-    return
-  } catch (e) {
-    expect(e).to.be.instanceOf(Error)
-    expect(e.message).to.eql('Invalid ammount in split destination')
-  }
-})
+//   try {
+//     await split(
+//       alicePrivateKey,
+//       utils.getUtxo(issueTxid, issueTx, 0),
+//       splitDestinations,
+//       utils.getUtxo(issueTxid, issueTx, 2),
+//       fundingPrivateKey
+//     )
+//     expect(false).toBeTruthy()
+//     return
+//   } catch (e) {
+//     expect(e).to.be.instanceOf(Error)
+//     expect(e.message).to.eql('Invalid ammount in split destination')
+//   }
+// })
 
-it('Split - Negative Integer Sats to Split Throws Error', async () => {
-  const splitDestinations = []
-  splitDestinations[0] = { address: bobAddr, satoshis: -15000 }
-  splitDestinations[1] = { address: bobAddr, satoshis: 15000 }
+// it('Split - Negative Integer Sats to Split Throws Error', async () => {
+//   const splitDestinations = []
+//   splitDestinations[0] = { address: bobAddr, satoshis: -15000 }
+//   splitDestinations[1] = { address: bobAddr, satoshis: 15000 }
 
-  try {
-    await split(
-      alicePrivateKey,
-      utils.getUtxo(issueTxid, issueTx, 0),
-      splitDestinations,
-      utils.getUtxo(issueTxid, issueTx, 2),
-      fundingPrivateKey
-    )
-    expect(false).toBeTruthy()
-    return
-  } catch (e) {
-    expect(e).to.be.instanceOf(Error)
-    expect(e.message).to.eql('Invalid ammount in split destination')
-  }
-})
+//   try {
+//     await split(
+//       alicePrivateKey,
+//       utils.getUtxo(issueTxid, issueTx, 0),
+//       splitDestinations,
+//       utils.getUtxo(issueTxid, issueTx, 2),
+//       fundingPrivateKey
+//     )
+//     expect(false).toBeTruthy()
+//     return
+//   } catch (e) {
+//     expect(e).to.be.instanceOf(Error)
+//     expect(e.message).to.eql('Invalid ammount in split destination')
+//   }
+// })
 
-it('Split - Sending to Issuer Throws Error', async () => {
-  const bobAmount1 = issueTx.vout[0].value / 2
-  const bobAmount2 = issueTx.vout[0].value - bobAmount1
-  const issuerAddress = issuerPrivateKey.toAddress(process.env.NETWORK).toString()
-  const splitDestinations = []
-  splitDestinations[0] = { address: issuerAddress, satoshis: bitcoinToSatoshis(bobAmount1) }
-  splitDestinations[1] = { address: bobAddr, satoshis: bitcoinToSatoshis(bobAmount2) }
-  try {
-    await split(
-      alicePrivateKey,
-      utils.getUtxo(issueTxid, issueTx, 0),
-      splitDestinations,
-      utils.getUtxo(issueTxid, issueTx, 2),
-      fundingPrivateKey
-    )
-    expect(false).toBeTruthy()
-    return
-  } catch (e) {
-    expect(e).to.be.instanceOf(Error)
-    expect(e.message).to.eql('Token UTXO cannot be sent to issuer address')
-  }
-})
+// it('Split - Sending to Issuer Throws Error', async () => {
+//   const bobAmount1 = issueTx.vout[0].value / 2
+//   const bobAmount2 = issueTx.vout[0].value - bobAmount1
+//   const issuerAddress = issuerPrivateKey.toAddress(process.env.NETWORK).toString()
+//   const splitDestinations = []
+//   splitDestinations[0] = { address: issuerAddress, satoshis: bitcoinToSatoshis(bobAmount1) }
+//   splitDestinations[1] = { address: bobAddr, satoshis: bitcoinToSatoshis(bobAmount2) }
+//   try {
+//     await split(
+//       alicePrivateKey,
+//       utils.getUtxo(issueTxid, issueTx, 0),
+//       splitDestinations,
+//       utils.getUtxo(issueTxid, issueTx, 2),
+//       fundingPrivateKey
+//     )
+//     expect(false).toBeTruthy()
+//     return
+//   } catch (e) {
+//     expect(e).to.be.instanceOf(Error)
+//     expect(e.message).to.eql('Token UTXO cannot be sent to issuer address')
+//   }
+// })
 
-it('Split - Address Too Long Throws Error', async () => {
-  const bobAmount1 = issueTx.vout[0].value / 2
-  const bobAmount2 = issueTx.vout[0].value - bobAmount1
-  console.log(bobAddr)
-  const splitDestinations = []
-  splitDestinations[0] = { address: '1LF2wNCBT9dp5jN7fa6xSAaUGjJ5Pyz5VGaUG', satoshis: bitcoinToSatoshis(bobAmount1) }
-  splitDestinations[1] = { address: bobAddr, satoshis: bitcoinToSatoshis(bobAmount2) }
-  try {
-    await split(
-      alicePrivateKey,
-      utils.getUtxo(issueTxid, issueTx, 0),
-      splitDestinations,
-      utils.getUtxo(issueTxid, issueTx, 2),
-      fundingPrivateKey
-    )
-    expect(false).toBeTruthy()
-    return
-  } catch (e) {
-    expect(e).to.be.instanceOf(Error)
-    expect(e.message).to.eql('Invalid Address in split destination')
-  }
-})
+// it('Split - Address Too Long Throws Error', async () => {
+//   const bobAmount1 = issueTx.vout[0].value / 2
+//   const bobAmount2 = issueTx.vout[0].value - bobAmount1
+//   console.log(bobAddr)
+//   const splitDestinations = []
+//   splitDestinations[0] = { address: '1LF2wNCBT9dp5jN7fa6xSAaUGjJ5Pyz5VGaUG', satoshis: bitcoinToSatoshis(bobAmount1) }
+//   splitDestinations[1] = { address: bobAddr, satoshis: bitcoinToSatoshis(bobAmount2) }
+//   try {
+//     await split(
+//       alicePrivateKey,
+//       utils.getUtxo(issueTxid, issueTx, 0),
+//       splitDestinations,
+//       utils.getUtxo(issueTxid, issueTx, 2),
+//       fundingPrivateKey
+//     )
+//     expect(false).toBeTruthy()
+//     return
+//   } catch (e) {
+//     expect(e).to.be.instanceOf(Error)
+//     expect(e.message).to.eql('Invalid Address in split destination')
+//   }
+// })
 
-it('Split - Null  STAS UTXO Throws Error', async () => {
-  const bobAmount1 = issueTx.vout[0].value / 2
-  const bobAmount2 = issueTx.vout[0].value - bobAmount1
-  const splitDestinations = []
-  splitDestinations[0] = { address: bobAddr, satoshis: bitcoinToSatoshis(bobAmount1) }
-  splitDestinations[1] = { address: bobAddr, satoshis: bitcoinToSatoshis(bobAmount2) }
-  try {
-    await split(
-      alicePrivateKey,
-      null,
-      splitDestinations,
-      utils.getUtxo(issueTxid, issueTx, 2),
-      fundingPrivateKey
-    )
-    expect(false).toBeTruthy()
-    return
-  } catch (e) {
-    expect(e).to.be.instanceOf(Error)
-    expect(e.message).to.eql('stasUTxo is null')
-  }
-})
+// it('Split - Null  STAS UTXO Throws Error', async () => {
+//   const bobAmount1 = issueTx.vout[0].value / 2
+//   const bobAmount2 = issueTx.vout[0].value - bobAmount1
+//   const splitDestinations = []
+//   splitDestinations[0] = { address: bobAddr, satoshis: bitcoinToSatoshis(bobAmount1) }
+//   splitDestinations[1] = { address: bobAddr, satoshis: bitcoinToSatoshis(bobAmount2) }
+//   try {
+//     await split(
+//       alicePrivateKey,
+//       null,
+//       splitDestinations,
+//       utils.getUtxo(issueTxid, issueTx, 2),
+//       fundingPrivateKey
+//     )
+//     expect(false).toBeTruthy()
+//     return
+//   } catch (e) {
+//     expect(e).to.be.instanceOf(Error)
+//     expect(e.message).to.eql('stasUTxo is null')
+//   }
+// })
 
-it('Split - Null Split Addresses Throws Error', async () => {
-  try {
-    await split(
-      alicePrivateKey,
-      utils.getUtxo(issueTxid, issueTx, 0),
-      null,
-      utils.getUtxo(issueTxid, issueTx, 2),
-      fundingPrivateKey
-    )
-    expect(false).toBeTruthy()
-    return
-  } catch (e) {
-    expect(e).to.be.instanceOf(Error)
-    expect(e.message).to.eql('split destinations array is null or empty')
-  }
-})
+// it('Split - Null Split Addresses Throws Error', async () => {
+//   try {
+//     await split(
+//       alicePrivateKey,
+//       utils.getUtxo(issueTxid, issueTx, 0),
+//       null,
+//       utils.getUtxo(issueTxid, issueTx, 2),
+//       fundingPrivateKey
+//     )
+//     expect(false).toBeTruthy()
+//     return
+//   } catch (e) {
+//     expect(e).to.be.instanceOf(Error)
+//     expect(e.message).to.eql('split destinations array is null or empty')
+//   }
+// })
 
-it('Split - Null Funding Private Key Throws Error', async () => {
-  const bobAmount1 = Math.floor(issueTx.vout[0].value / 2)
-  const bobAmount2 = issueTx.vout[0].value - bobAmount1
-  const splitDestinations = []
-  splitDestinations[0] = { address: bobAddr, satoshis: bitcoinToSatoshis(bobAmount1) }
-  splitDestinations[1] = { address: bobAddr, satoshis: bitcoinToSatoshis(bobAmount2) }
-  try {
-    await split(
-      alicePrivateKey,
-      utils.getUtxo(issueTxid, issueTx, 0),
-      splitDestinations,
-      utils.getUtxo(issueTxid, issueTx, 2),
-      null
-    )
-    expect(false).toBeTruthy()
-    return
-  } catch (e) {
-    expect(e).to.be.instanceOf(Error)
-    expect(e.message).to.eql('Payment UTXO provided but payment key is null')
-  }
-})
+// it('Split - Null Funding Private Key Throws Error', async () => {
+//   const bobAmount1 = Math.floor(issueTx.vout[0].value / 2)
+//   const bobAmount2 = issueTx.vout[0].value - bobAmount1
+//   const splitDestinations = []
+//   splitDestinations[0] = { address: bobAddr, satoshis: bitcoinToSatoshis(bobAmount1) }
+//   splitDestinations[1] = { address: bobAddr, satoshis: bitcoinToSatoshis(bobAmount2) }
+//   try {
+//     await split(
+//       alicePrivateKey,
+//       utils.getUtxo(issueTxid, issueTx, 0),
+//       splitDestinations,
+//       utils.getUtxo(issueTxid, issueTx, 2),
+//       null
+//     )
+//     expect(false).toBeTruthy()
+//     return
+//   } catch (e) {
+//     expect(e).to.be.instanceOf(Error)
+//     expect(e.message).to.eql('Payment UTXO provided but payment key is null')
+//   }
+// })
 
-async function setup() {
-  issuerPrivateKey = bsv.PrivateKey()
-  fundingPrivateKey = bsv.PrivateKey()
-  contractUtxos = await getFundsFromFaucet(issuerPrivateKey.toAddress(process.env.NETWORK).toString())
-  fundingUtxos = await getFundsFromFaucet(fundingPrivateKey.toAddress(process.env.NETWORK).toString())
-  publicKeyHash = bsv.crypto.Hash.sha256ripemd160(issuerPrivateKey.publicKey.toBuffer()).toString('hex')
-  bobPrivateKey = bsv.PrivateKey()
-  alicePrivateKey = bsv.PrivateKey()
-  bobAddr = bobPrivateKey.toAddress(process.env.NETWORK).toString()
-  aliceAddr = alicePrivateKey.toAddress(process.env.NETWORK).toString()
+// async function setup() {
+//   issuerPrivateKey = bsv.PrivateKey()
+//   fundingPrivateKey = bsv.PrivateKey()
+//   contractUtxos = await getFundsFromFaucet(issuerPrivateKey.toAddress(process.env.NETWORK).toString())
+//   fundingUtxos = await getFundsFromFaucet(fundingPrivateKey.toAddress(process.env.NETWORK).toString())
+//   publicKeyHash = bsv.crypto.Hash.sha256ripemd160(issuerPrivateKey.publicKey.toBuffer()).toString('hex')
+//   bobPrivateKey = bsv.PrivateKey()
+//   alicePrivateKey = bsv.PrivateKey()
+//   bobAddr = bobPrivateKey.toAddress(process.env.NETWORK).toString()
+//   aliceAddr = alicePrivateKey.toAddress(process.env.NETWORK).toString()
 
-  const symbol = 'TAALT'
-  const supply = 10000
-  const schema = utils.schema(publicKeyHash, symbol, supply)
+//   const symbol = 'TAALT'
+//   const supply = 10000
+//   const schema = utils.schema(publicKeyHash, symbol, supply)
 
-  const contractHex = await contract(
-    issuerPrivateKey,
-    contractUtxos,
-    fundingUtxos,
-    fundingPrivateKey,
-    schema,
-    supply
-  )
-  const contractTxid = await broadcast(contractHex)
-  const contractTx = await getTransaction(contractTxid)
+//   const contractHex = await contract(
+//     issuerPrivateKey,
+//     contractUtxos,
+//     fundingUtxos,
+//     fundingPrivateKey,
+//     schema,
+//     supply
+//   )
+//   const contractTxid = await broadcast(contractHex)
+//   const contractTx = await getTransaction(contractTxid)
 
-  const issueHex = await issue(
-    issuerPrivateKey,
-    utils.getIssueInfo(aliceAddr, 7000, bobAddr, 3000),
-    utils.getUtxo(contractTxid, contractTx, 0),
-    utils.getUtxo(contractTxid, contractTx, 1),
-    fundingPrivateKey,
-    true,
-    symbol,
-    2
-  )
-  issueTxid = await broadcast(issueHex)
-  issueTx = await getTransaction(issueTxid)
-}
+//   const issueHex = await issue(
+//     issuerPrivateKey,
+//     utils.getIssueInfo(aliceAddr, 7000, bobAddr, 3000),
+//     utils.getUtxo(contractTxid, contractTx, 0),
+//     utils.getUtxo(contractTxid, contractTx, 1),
+//     fundingPrivateKey,
+//     true,
+//     symbol,
+//     2
+//   )
+//   issueTxid = await broadcast(issueHex)
+//   issueTx = await getTransaction(issueTxid)
+// }
